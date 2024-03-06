9e9fcaeed7cb4e3b452586e8ce2410a638fcb2d7
Austin Yang <a.yang082106@gmail.com>
2024-03-05 23:44:40 -0500

    button controls

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,13 @@ import logo from './logo.png'
 import robot from './placeholder.jpg'
 import placeholder_128 from './128x128placeholder.png'
 import placeholder_96 from './96x96placeholder.png'
-// import intake from './intake.png'
-// import shooter from './shooter.png'
-// import climber from './climber.png'
+import intake from './intake.png'
+import shooter from './shooter.png'
+import climber from './climber.png'
 import climber_close from './climber_close.png'
 
 function App() {
-  // const images = [
-  //   intake,
-  //   shooter,
-  //   climber,
-  //   robot
-  // ];
-  function onClick(){
-    return ;
-  }
+  const [robotImage, setRobotImage] = React.useState(robot);
   return (
     <div className="App">
     <head>
@@ -35,12 +27,13 @@ function App() {
       </div>
     </div>
     <div className="mid_align">
-      <img className="img1" src={robot} alt='robot'/>
-      <div className='right_card'>
-      <input type='image' src={climber_close} alt='climber button'></input>
-      <input type='image' src={placeholder_96} alt='intake button'></input>
-      <input type='image' src={placeholder_96} alt='shooter button'></input>
-    </div>
+      <img className="img1" src={robotImage} alt='robot'/>
+          <div className='right_card'>
+            <input type='image' src={climber_close} alt='climber button' onClick={() => setRobotImage(climber)}></input>
+            <input type='image' src={placeholder_96} alt='intake button' onClick={() => setRobotImage(intake)}></input>
+            <input type='image' src={placeholder_96} alt='shooter button'onClick={() => setRobotImage(shooter)}></input>
+          </div>
+          
     </div>
   </div>
   );
